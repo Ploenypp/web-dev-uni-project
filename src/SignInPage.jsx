1af450c2e9aca0ce4bd6957160f6c31f36e835bd
Ploenypp <everhonorlight@gmail.com>
2025-03-21 12:39:34 +0100

    signin page
    elements made
    customization needed
    routing + functions needed

--- src/SignInPage.jsx
+++ src/SignInPage.jsx
@@ -1,10 +1,34 @@
 import {useState, useEffect, useRef} from 'react';
+import "./SignIn.css";
+import logo from './assets/logo.png';
 
 function SignInPage() {
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
     const [isLoading,setLoading] = useState(false);
-    const [error,setError] = 
+    const [error,setError] = useState("");
+
+    function onSignIn(username,password) {
+    }
+
+    return(<div className ="SignInPage">
+        <div id="logo">
+            <img src={logo} 
+            alt="Organiz'asso Logo"/>
+        </div>
+
+        <form>
+            <label htmlFor="username">Nom d'utilisateur</label>
+            <input id="username" type="text" class="text-input"/>
+            <br></br>
+
+            <label htmlFor="password">Mot de passe</label>
+            <input id="password" type="text" class="text-input"/>
+        </form> 
+        <button id="login" type="button" onClick={onSignIn(username,password)}>Connexion</button>
+
+        <button id="register" type="button">Inscription</button>
+    </div>)
 }
 
 export default SignInPage;
