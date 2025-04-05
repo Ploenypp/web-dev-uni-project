f376aaf10b91dfdda4ae5937ecbb564d22dc6d12
Ploenypp <everhonorlight@gmail.com>
2025-04-06 01:02:32 +0200

    created Post component!
    I still haven't started MongoDB so I had ChatGPT generate me some dummy posts to visualise how it would look.
    
    Will need to change the timestamp to be an actual time, though.

---
+++ src/objects/Post.jsx
@@ -0,0 +1,20 @@
+import {useState, useEffect, useRef} from 'react';
+
+import "../styles/Post.css";
+
+function Post(props) {
+
+    return(<div className="Post">
+        <div>
+            <div id="post_title"><strong>{props.title}</strong></div>
+            <div id="post_info">
+                by {props.author}, {props.timestamp}
+            </div>
+        </div>
+        <div id="post_content">
+            {props.content}
+        </div>
+    </div>)
+}
+
+export default Post;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,10 +2,65 @@ import {useState, useEffect, useRef} from 'react';
 
 import tmp_pfp from "../assets/tmp_pfp.png";
 import "../styles/ProfilePage.css";
+
 import Ribbon from "../objects/Ribbon.jsx";
 import Searchbar from "../objects/Searchbar.jsx"
+import Post from "../objects/Post.jsx"
 
 function ProfilePage() {
+    const [role,setRole] = useState("");
+    const [teams,setTeams] = useState("");
+    const [interests,setInterests] = useState("");
+
+    const editRole = (evt) => setRole(evt.target.value);
+    const editTeams = (evt) => setTeams(evt.target.value);
+    const editInterests = (evt) => setInterests(evt.target.value);
+
+    const dummyPosts = [
+        {
+          title: "The Egg Incident",
+          author: "Axelle",
+          timestamp: "2 hours ago",
+          content: "So I tried cracking an egg with one hand like the cool chefs do and it ended up on the floor, the counter, my shirt, and somehow in my hair. 0/10 would not recommend without practice. RIP breakfast."
+        },
+        {
+          title: "Keyboard Crumbs",
+          author: "Axelle",
+          timestamp: "Yesterday",
+          content: "I cleaned my keyboard today and found what could only be described as an entire ecosystem. There was a sesame seed under the 'G' key. I haven’t had a sesame bagel in months. Who’s responsible for this??? (It's me. I'm responsible.)"
+        },
+        {
+          title: "The Great Pasta Spill",
+          author: "Axelle",
+          timestamp: "3 days ago",
+          content: "Was cooking pasta. Tried to be fancy and toss it in the pan like they do on TikTok. Half the pasta landed back in the pan. The other half is now a crunchy floor garnish. My cat is delighted."
+        },
+        {
+          title: "Phantom Notifications",
+          author: "Axelle",
+          timestamp: "Last week",
+          content: "Does anyone else hear the *phantom ping*? That faint imaginary notification sound that makes you check your phone for absolutely no reason? I think my brain is trying to gaslight me. Rude."
+        },
+        {
+          title: "Tomato Soup Chaos",
+          author: "Axelle",
+          timestamp: "2 weeks ago",
+          content: "Decided to make tomato soup from scratch. Accidentally doubled the garlic. Burned my tongue. Added cream to cool it down. Forgot lactose intolerance. Would I do it again? Probably. But I’d regret it less if I wore gloves while chopping chili."
+        },
+        {
+          title: "Night Owl Strikes Again",
+          author: "Axelle",
+          timestamp: "2:47 AM",
+          content: "Why do I suddenly feel the urge to reorganize my closet at nearly 3 in the morning? My productivity makes no sense and I fear this is becoming a lifestyle. Send snacks. And maybe a nap schedule."
+        },
+        {
+          title: "Caught Singing in Public",
+          author: "Axelle",
+          timestamp: "1 month ago",
+          content: "Had headphones in. Thought I was humming quietly. Apparently I was full-volume belting out a dramatic anime opening in the supermarket. Someone applauded. Not sure if it was sarcastic or sincere. Still bowed. No regrets."
+        }
+      ];
+
     return(<div className="ProfilePage">
         <Ribbon />
         <div id="pf_container">
@@ -21,11 +76,20 @@ function ProfilePage() {
                 </div>
                 <button id="edit" type="button">Modifier</button>
             </div>
-            <div className="profile_search">
-                <Searchbar />
-            </div>
-            <div className="profile_posts">
-                
+            <div id="pf_subcontainer">
+                <div className="profile_search">
+                    <Searchbar />
+                </div>
+                <div className="profile_posts">
+                    {dummyPosts.map((post, index) => (
+                    <Post
+                        key={index}
+                        title={post.title}
+                        author={post.author}
+                        timestamp={post.timestamp}
+                        content={post.content}
+                    />))}
+                </div>
             </div>
         </div>
     </div>)
