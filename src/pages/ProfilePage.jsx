4886321f968bad6f009ff624246df6047eec50ab
Ploenypp <everhonorlight@gmail.com>
2025-04-06 00:15:40 +0200

    place searchbar relative to profile sidebar

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,24 +3,29 @@ import {useState, useEffect, useRef} from 'react';
 import tmp_pfp from "../assets/tmp_pfp.png";
 import "../styles/ProfilePage.css";
 import Ribbon from "../objects/Ribbon.jsx";
+import Searchbar from "../objects/Searchbar.jsx"
 
 function ProfilePage() {
     return(<div className="ProfilePage">
         <Ribbon />
-        <div id="profile_sidebar">
-            <img src={tmp_pfp} id="profile_pic" alt="profile picture"/>
-            <div id="profile_text">
-                <p>Prénom : <strong>Axelle</strong> </p> 
-                <p>Nom : <strong>Delacroix</strong></p>
-                <p>Date de naissance : <strong>28 March 2002</strong> </p> 
+        <div id="pf_container">
+            <div id="profile_sidebar">
+                <img src={tmp_pfp} id="profile_pic" alt="profile picture"/>
+                <div id="profile_text">
+                    <p>Prénom : <strong>Axelle</strong> </p> 
+                    <p>Nom : <strong>Delacroix</strong></p>
+                    <p>Date de naissance : <strong>28 March 2002</strong> </p> 
+                    <p>Rôle : <strong>KeyBlade Wielder</strong> </p> 
+                    <p>Équipe(s) : <strong>Heart Collection</strong></p>
+                    <p>Intérêts : <strong>Music</strong> </p>
+                </div>
+                <button id="edit" type="button">Modifier</button>
             </div>
-            <div id="profile_text">
-                <p>Rôle : <strong>KeyBlade Wielder</strong> </p> 
-                <p>Équipe(s) : <strong>Heart Collection</strong></p>
+            <div className="profile_search">
+                <Searchbar />
             </div>
-            <div id="profile_text">
-                <p>Intérêts : <strong>KeyBlade Wielder</strong> </p> 
-                <p>Équipe(s) : <strong>Heart Collection</strong></p>
+            <div className="profile_posts">
+                
             </div>
         </div>
     </div>)
