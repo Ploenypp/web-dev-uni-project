23a76836bb86866d463960f2b69c9bb4913dd5b2
Ploenypp <everhonorlight@gmail.com>
2025-04-18 12:40:00 +0200

    change routing for ProfileInfo
    That way, we can reuse the ProfileInfo component for other user profiles and not just user's profile page.

--- client/src/objects/ProfileInfo.jsx
+++ client/src/objects/ProfileInfo.jsx
@@ -3,24 +3,15 @@ import {useState, useEffect, useRef} from 'react';
 import tmp_pfp from "../assets/tmp_pfp.png";
 import "../styles/ProfilePage.css";
 
-function ProfileInfo() {
-    const [userInfo, setUserInfo] = useState("");
-
-    useEffect(() => {
-        fetch('http://localhost:8000/api/user/profile', { credentials: 'include' })
-            .then(res => res.json())
-            .then(data => setUserInfo(data))
-            .catch(err => console.error("Error fetching user data:", err));
-    }, []);
-
+function ProfileInfo(props) {
     return(<div className="ProfileInfo">
         <img src={tmp_pfp} id="profile_pic" alt="profile picture"/>
                 <div id="profile_text">
-                    <p>Prénom : <strong>{userInfo.fstname}</strong> </p> 
-                    <p>Nom : <strong>{userInfo.surname}</strong></p>
-                    <p>Date de naissance : <strong>{userInfo.dob}</strong> </p> 
-                    <p>Status : <strong>{userInfo.status}</strong> </p> 
-                    <p>Équipe(s) : <strong>{userInfo.team}</strong></p>
+                    <p>Prénom : <strong>{props.fstname}</strong> </p> 
+                    <p>Nom : <strong>{props.surname}</strong></p>
+                    <p>Date de naissance : <strong>{props.dob}</strong> </p> 
+                    <p>Status : <strong>{props.status}</strong> </p> 
+                    <p>Équipe(s) : <strong>{props.team}</strong></p>
                 </div>
     </div>)
 }
