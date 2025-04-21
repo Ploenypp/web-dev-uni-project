0c2d1dea386aa0a37a83a87ad771ab0680eb3994
Ploenypp <everhonorlight@gmail.com>
2025-04-21 02:34:26 +0200

    added profile pics for requests

--- client/src/objects/Request.jsx
+++ client/src/objects/Request.jsx
@@ -3,6 +3,23 @@ import axios from 'axios';
 
 import "../styles/Requests.css";
 
+// profile pictures
+import tmp_pfp from "../assets/tmp_pfp.png";
+import xemnas from "../assets/profile_pics/xemnas.png";
+import xigbar from "../assets/profile_pics/xigbar.png";
+import xaldin from "../assets/profile_pics/xaldin.png";
+import vexen from "../assets/profile_pics/vexen.png";
+import lexaeus from "../assets/profile_pics/lexaeus.png";
+import zexion from "../assets/profile_pics/zexion.png";
+import saix from "../assets/profile_pics/saix.png";
+import axel from "../assets/profile_pics/axel.png";
+import demyx from "../assets/profile_pics/demyx.png";
+import luxord from "../assets/profile_pics/luxord.png";
+import marluxia from "../assets/profile_pics/marluxia.png";
+import larxene from "../assets/profile_pics/larxene.png";
+import roxas from "../assets/profile_pics/roxas.png";
+import xion from "../assets/profile_pics/xion.png";
+
 function Request(props) {
     const friendID = props.senderID;
 
@@ -26,9 +43,27 @@ function Request(props) {
         }
     }
 
+    const pfp = (name) => {
+        if (name === "Xemnas") { return xemnas; }
+        if (name === "Xigbar") { return xigbar; }
+        if (name === "Xaldin") { return xaldin; }
+        if (name === "Vexen") { return vexen; }
+        if (name === "Lexaeus") { return lexaeus; }
+        if (name === "Zexion") { return zexion; }
+        if (name === "Saix") { return saix; }
+        if (name === "Axel") { return axel; }
+        if (name === "Demyx") { return demyx; }
+        if (name === "Luxord") { return luxord; }
+        if (name === "Marluxia") { return marluxia; }
+        if (name === "Larxene") { return larxene; }
+        if (name === "Roxas") { return roxas; }
+        if (name === "Xion") { return xion; }
+        return tmp_pfp;
+    }
 
     return(<div className="Request">
-        {props.fst_name} {props.surname}
+        <img src={pfp(props.fst_name)} id="req_pfp" alt="profile picture"/>
+        <p> {props.fst_name} {props.surname}</p>
         <div id="req_btns">
             <button id="accept" type="button" onClick={handleAccept}>✓</button>
             <button id="reject" type="button" onClick={handleReject}>X</button>
